e71083c660cb835205d43cca64c747073e1bc536
jordan <jordan@hakustudios.com>
2019-05-28 21:40:48 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const forecast = (longitude, latitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/623e65b79ebb12ecd7ae278df1e1a138/'+ encodeURIComponent(latitude) +',' + encodeURIComponent(longitude) + '?units=si'
+
+    request({ url, json: true }, (error, {body}) => {
+        if(error) {
+            callback('Unable to connect to weather service.', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, {
+                temperature: body.currently.temperature,
+                rainChance: body.currently.precipProbability,
+                summary: body.daily.data[0].summary
+            })
+        }
+    })
+}
+
+module.exports = forecast
